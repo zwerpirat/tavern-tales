16795792e4235b8fae0fe78a0d60614d3cb4e732
zwergpirat <zwerg0pirat@gmail.com>
2025-02-27 12:00:35 +0100

    added short description in readme file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # tavern-tales
+
+A safespace for all your NPCs of your D&D campaign. Finally no NPC gets lost along the way and your players can remember their names, locations and descriptions. Yay! No more hours spent on creating new NPCs, just for your players to forget them, ignore them or try to kill them without any reason. Every NPC is important, because we are all someone else's NPC, right? 
--- server/src/routers.ts
+++ server/src/routers.ts
@@ -1,15 +1,13 @@
 import express, { Request, Response, Router } from 'express';
 import NPC from './models/npc';
 
-
 const router: Router = express.Router();
 
-
 // Define 4 Routes for NPCs: 
-    // /npc (GET): show all npcs in database
-    // /npc (POST): add an npc to database
-    // /npc (POST): add an npc to favorites list
-    // /npc/:npcId (DELETE): remove npc from favorites list
+// /npc (GET): show all npcs in database
+// /npc (POST): add an npc to database
+// /npc (POST): add an npc to favorites list
+// /npc/:npcId (DELETE): remove npc from favorites list
 
 // get all the npcs stored in the database
 router.get('/npc', async (req: Request, res: Response) => {
@@ -29,25 +27,25 @@ router.get('/npc', async (req: Request, res: Response) => {
 router.post('/npc', async (req: Request, res: Response) => {
     try {
         // error handling if any required parameter is missing as an input from the user
-        const missingParameters = []; 
-        if(!req.body.name) missingParameters.push('name');
-        if(!req.body.race) missingParameters.push('race');
-        if(!req.body.location) missingParameters.push('location');
-        if(!req.body.description) missingParameters.push('description');
+        const missingParameters = [];
+        if (!req.body.name) missingParameters.push('name');
+        if (!req.body.race) missingParameters.push('race');
+        if (!req.body.location) missingParameters.push('location');
+        if (!req.body.description) missingParameters.push('description');
 
-        if(missingParameters.length > 0){
+        if (missingParameters.length > 0) {
             res.status(400).json({
                 error: 'Missing some required input, please fill in all the fields',
                 missingParameters
             });
-        // if no parameters is missing, new NPC is created   
+            // if no parameters is missing, new NPC is created   
         } else {
             const npc = await NPC.create(req.body);
             await npc.save();
             res.status(201).json(npc);
         }
     } catch (error) {
-        console.log(error);
+        res.status(500).json(error);
     }
 });
 
@@ -56,7 +54,7 @@ router.delete('/npc/:npcId', async (req: Request<{ npcId: number }>, res: Respon
     const npcId: number = req.params.npcId;
     try {
         const deleteCount = NPC.destroy({ where: { id: npcId } });
-        res.json({deleteCount});
+        res.json({ deleteCount });
     } catch (error) {
         console.log(error);
     }
